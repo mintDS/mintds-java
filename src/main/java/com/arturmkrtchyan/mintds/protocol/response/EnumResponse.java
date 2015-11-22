b8b7cbdddd858b1709ef0c8d21d07b2ad453ff2d
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2015-11-22 14:52:26 +0100

    Deleted protocol files from client

--- src/main/java/com/arturmkrtchyan/mintds/protocol/response/EnumResponse.java
+++
@@ -1,9 +0,0 @@
-package com.arturmkrtchyan.mintds.protocol.response;
-
-public enum EnumResponse implements Response {
-    SUCCESS,
-    EXISTS,
-    YES,
-    NO,
-    NON_EXISTENT
-}
--- src/main/java/com/arturmkrtchyan/mintds/protocol/response/Response.java
+++
@@ -1,6 +0,0 @@
-package com.arturmkrtchyan.mintds.protocol.response;
-
-public interface Response {
-
-
-}
