d9a2f55a542d4c050007c9e724f0239fc90f5790
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2015-12-01 22:34:11 +0100

    Added BloomFilter client facade

---
+++ src/main/java/com/arturmkrtchyan/mintds/BloomFilter.java
@@ -0,0 +1,25 @@
+package com.arturmkrtchyan.mintds;
+
+public class BloomFilter {
+
+    public boolean create(final String filter) {
+        return false;
+    }
+
+    public boolean exists(final String filter) {
+        return false;
+    }
+
+    public boolean add(final String filter, final String value) {
+        return false;
+    }
+
+    public boolean contains(final String filter, final String value) {
+        return false;
+    }
+
+    public boolean drop(final String filter) {
+        return false;
+    }
+
+}
